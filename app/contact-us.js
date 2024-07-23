d5d05607ae6be8e5dcd6332594d92d76db5291c1
a2k <arash.khataei@gmail.com>
2024-07-24 01:43:20 +0200

    add redirects for main paths

---
+++ app/contact-us.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Redirect } from 'expo-router';
+
+// This only redirects to the fa language as main language
+export default function IndexPage() {
+  return <Redirect href="/fa/contact-us" />;
+  }
